d856a5697cd3edf2295b282b94b3bc94c934667b
mnky9800n <johnm.aiken@gmail.com>
2025-06-22 23:48:24 -0500

    changed cheksum

--- Formula/windowfied.rb
+++ Formula/windowfied.rb
@@ -2,7 +2,7 @@ class Windowfied < Formula
   desc "Windows-style commands for macOS"
   homepage "https://github.com/mnky9800n/homebrew-tools"
   url "https://github.com/mnky9800n/homebrew-tools/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "4daac648fe3816211402059d9b6337b1742edf7d65edf6951ea7fe5887819d7c"
+  sha256 "3e7e656643564b457a3d42bf33c961c12f3042509f650f1a5a6b495a6c6828aa"
   license "MIT"
 
   def install
