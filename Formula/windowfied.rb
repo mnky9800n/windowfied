1ec576ec6ab7bb6d6f82130a84e4a9bbcd4c4ccd
mnky9800n <johnm.aiken@gmail.com>
2025-06-22 23:46:46 -0500

    Changed urls

--- Formula/windowfied.rb
+++ Formula/windowfied.rb
@@ -1,7 +1,7 @@
 class Windowfied < Formula
   desc "Windows-style commands for macOS"
-  homepage "https://github.com/mnky9800n/windowfied"
-  url "https://github.com/mnky9800n/windowfied/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/mnky9800n/homebrew-tools"
+  url "https://github.com/mnky9800n/homebrew-tools/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "4daac648fe3816211402059d9b6337b1742edf7d65edf6951ea7fe5887819d7c"
   license "MIT"
 
